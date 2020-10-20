0012c8d76c2f31e95509ec9d974f7832c7d09a47
RaphaelCarvalho <p.raphael.carvalho@hotmail.com>
2020-10-19 23:25:58 -0300

    Adicionado parametro de input da url

--- download.js
+++ download.js
@@ -1,17 +1,22 @@
 const fs = require('fs')
 const youtubedl = require('youtube-dl')
 
-const video = youtubedl('https://youtu.be/mLDz8ymV9QM',
-  // Optional arguments passed to youtube-dl.
-  ['--format=18'],
+//Parametro para coletar o argumento ao executar o script:
+var url = process.argv[2];
+
+const video = youtubedl(url,
+// Optional arguments passed to youtube-dl.
+  	['--format=18'],
   // Additional options can be given for calling `child_process.execFile()`.
-  { cwd: __dirname })
+	{ cwd: __dirname })
  
 // Will be called when the download starts.
 video.on('info', function(info) {
   console.log('Download started')
   console.log('filename: ' + info._filename)
   console.log('size: ' + info.size)
-})
+//})
 
-video.pipe(fs.createWriteStream('myvideo.mp4'))
+video.pipe(fs.createWriteStream(info._filename))
+//video.pipe(fs.createWriteStream('myvideo.mp4'))
+})
